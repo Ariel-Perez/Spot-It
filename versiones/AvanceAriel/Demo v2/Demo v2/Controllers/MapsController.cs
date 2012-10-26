7e9508999928f0689eeba82bfb62ed6765955629
Gustavo Saavedra <glsaaved@uc.cl>
2012-10-26 19:00:00 -0300

    nueva version con el avance de ariel
    esta en una nueva carpeta que se llama versiones

---
+++ versiones/AvanceAriel/Demo v2/Demo v2/Controllers/MapsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_v2.Controllers
+{
+    public class MapsController : Controller
+    {
+        //
+        // GET: /Maps/
+
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Main map";
+            return View();
+        }
+
+    }
+}
