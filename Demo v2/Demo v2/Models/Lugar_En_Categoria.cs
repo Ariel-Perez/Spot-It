fdc78175501844d74a6abec7ba593fc132e921c1
Katherine Páez Ramos <slakat@gmail.com>
2012-10-26 22:06:22 -0300

    Reemplazo carpeta
    con proyecto de Ariel

---
+++ Demo v2/Demo v2/Models/Lugar_En_Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace Demo_v2.Models
+{
+    public class Lugar_En_Categoria
+    {
+        [Key, Column(Order = 0)]
+        public int idMapa { get; set; }
+        [Key, Column(Order = 1)]
+        public int idZona { get; set; }
+        [Key, Column(Order = 2)]
+        public string nombreCategoria { get; set; }
+    }
+}
---
+++ Demo v2/Demo v2/Models/SpotItEntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Demo_v2.Models
+{
+    public class SpotItEntities : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Lugar> Lugar { get; set; }
+        public DbSet<Lugar_En_Categoria> Lugar_En_Categoria { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
