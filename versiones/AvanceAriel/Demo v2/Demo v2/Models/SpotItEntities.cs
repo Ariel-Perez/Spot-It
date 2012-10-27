508c3601f409580e2a12ab1146abdffb01e8a64e
Gustavo Saavedra <glsaaved@uc.cl>
2012-10-26 22:58:36 -0300

    parseando datos

--- versiones/AvanceAriel/Demo v2/Demo v2/Models/SpotItEntities.cs
+++ versiones/AvanceAriel/Demo v2/Demo v2/Models/SpotItEntities.cs
@@ -12,7 +12,22 @@ namespace Demo_v2.Models
         public DbSet<Categoria> Categoria { get; set; }
         public DbSet<Lugar> Lugar { get; set; }
         public DbSet<Lugar_En_Categoria> Lugar_En_Categoria { get; set; }
-
+        public DbSet<Autoridad> Autoridad { get; set; }
+        public DbSet<Asiste> Asiste { get; set; }
+        public DbSet<Badge> Badge { get; set; }
+        public DbSet<Comentario> Comentario { get; set; }
+        public DbSet<Complejo> Complejo { get; set; }
+        public DbSet<Construccion> Construccion { get; set; }
+        public DbSet<Departamento> Departamento { get; set; }
+        public DbSet<Evento>  Evento { get; set; }
+        public DbSet<Facultad> Facultad { get; set; }
+        public DbSet<Horarios> Horarios { get; set; }
+        public DbSet<Mapa> Mapa { get; set; }
+        public DbSet<Premio> Premio { get; set; }
+        public DbSet<Punto> Punto { get; set; }
+        public DbSet<Sala> Sala { get; set; }
+        public DbSet<TrabajaEn> TrabajaEn { get; set; }
+        public DbSet<Zona> Zona { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
