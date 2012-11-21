c54a5a8ce1eaa5795fb9aaf7721730b58810a981
Ariel Pérez Chávez <arielperezch@gmail.com>
2012-11-21 18:01:48 -0300

    Cambié los Checkbox e hice la página /Salas

---
+++ 16-11-2012/Demo v2 - copia/Demo v2/Controllers/SalasController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_v2.Controllers
+{
+    public class SalasController : Controller
+    {
+        //
+        // GET: /Salas/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
