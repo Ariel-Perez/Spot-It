fc1248f8c1098fc44cf20fc6ed629a9e7c64741a
Ariel Pérez Chávez <arielperezch@gmail.com>
2012-11-28 22:26:53 -0300

    It can now book rooms.

--- 16-11-2012/Demo v2 - copia/Demo v2/Controllers/SalasController.cs
+++ 16-11-2012/Demo v2 - copia/Demo v2/Controllers/SalasController.cs
@@ -27,7 +27,8 @@ namespace Demo_v2.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var h = db.Horario.First();
+            return View(h.fecha_ingreso);
         }
 
         //
@@ -93,6 +94,40 @@ namespace Demo_v2.Controllers
                 return View(new SolicitudReserva { Sala = sala, HorarioPedido = array });
             else
                 return View(default(Lugar));
+        }
+
+        public ActionResult ConfirmBooking(bool dia_1mod_1, bool dia_1mod_2, bool dia_1mod_3, bool dia_1mod_4, bool dia_1mod_5, bool dia_1mod_6, bool dia_1mod_7, bool dia_1mod_8,
+                                    bool dia_2mod_1, bool dia_2mod_2, bool dia_2mod_3, bool dia_2mod_4, bool dia_2mod_5, bool dia_2mod_6, bool dia_2mod_7, bool dia_2mod_8,
+                                    bool dia_3mod_1, bool dia_3mod_2, bool dia_3mod_3, bool dia_3mod_4, bool dia_3mod_5, bool dia_3mod_6, bool dia_3mod_7, bool dia_3mod_8,
+                                    bool dia_4mod_1, bool dia_4mod_2, bool dia_4mod_3, bool dia_4mod_4, bool dia_4mod_5, bool dia_4mod_6, bool dia_4mod_7, bool dia_4mod_8,
+                                    bool dia_5mod_1, bool dia_5mod_2, bool dia_5mod_3, bool dia_5mod_4, bool dia_5mod_5, bool dia_5mod_6, bool dia_5mod_7, bool dia_5mod_8,
+                                    bool dia_6mod_1, bool dia_6mod_2, bool dia_6mod_3, bool dia_6mod_4, bool dia_6mod_5, bool dia_6mod_6, bool dia_6mod_7, bool dia_6mod_8,
+                                    int idSala)
+        {
+            bool[][] array = new bool[6][];
+            array[0] = new bool[8] { dia_1mod_1, dia_1mod_2, dia_1mod_3, dia_1mod_4, dia_1mod_5, dia_1mod_6, dia_1mod_7, dia_1mod_8 };
+            array[1] = new bool[8] { dia_2mod_1, dia_2mod_2, dia_2mod_3, dia_2mod_4, dia_2mod_5, dia_2mod_6, dia_2mod_7, dia_2mod_8 };
+            array[2] = new bool[8] { dia_3mod_1, dia_3mod_2, dia_3mod_3, dia_3mod_4, dia_3mod_5, dia_3mod_6, dia_3mod_7, dia_3mod_8 };
+            array[3] = new bool[8] { dia_4mod_1, dia_4mod_2, dia_4mod_3, dia_4mod_4, dia_4mod_5, dia_4mod_6, dia_4mod_7, dia_4mod_8 };
+            array[4] = new bool[8] { dia_5mod_1, dia_5mod_2, dia_5mod_3, dia_5mod_4, dia_5mod_5, dia_5mod_6, dia_5mod_7, dia_5mod_8 };
+            array[5] = new bool[8] { dia_6mod_1, dia_6mod_2, dia_6mod_3, dia_6mod_4, dia_6mod_5, dia_6mod_6, dia_6mod_7, dia_6mod_8 };
+
+            var sala = db.Lugar.Single(s=>s.Id==idSala);
+            var horarios = db.Horario.Where(h => h.LugarId == idSala);
+
+            for (int i = 0; i < 6; i++)
+            {
+                string dia = dias[i + 1];
+                for (int j = 0; j < 8; j++)
+                {
+                    if(array[i][j] == false)
+                        continue;
+
+                    horarios.Single(h => h.Dia == dia && h.Modulo == j + 1).tipo_actividad = "Reservada";
+                }
+            }
+            db.SaveChanges();
+            return View(sala);
         }
     }
 }
